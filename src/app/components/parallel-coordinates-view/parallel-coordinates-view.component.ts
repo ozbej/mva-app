3c33ffa8314d1718bca15b6011c2fd6c480736d7
Ožbej Golob <ozbej.golob@gmail.com>
2023-05-27 14:05:33 +0200

    Minor change

--- src/app/components/parallel-coordinates-view/parallel-coordinates-view.component.ts
+++ src/app/components/parallel-coordinates-view/parallel-coordinates-view.component.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild, Renderer2, AfterViewInit, ElementRef } from '@angular/core';
 
 let data: any[] = [];
-for (let i = 0; i < 30000; i++) {
-  data.push(Array.from({length: 16}, () => Math.floor(Math.random() * 600)));
+for (let i = 0; i < 100; i++) {
+  data.push(Array.from({length: 20}, () => Math.floor(Math.random() * 600)));
 }
 
 @Component({
@@ -19,7 +19,7 @@ export class ParallelCoordinatesViewComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.ctx = this.canvas.nativeElement.getContext('2d');
 
-    this.renderer.setAttribute(this.canvas.nativeElement, 'width', '800');
+    this.renderer.setAttribute(this.canvas.nativeElement, 'width', '1800');
     this.renderer.setAttribute(this.canvas.nativeElement, 'height', '600');
 
     for (let line of data)
@@ -28,11 +28,11 @@ export class ParallelCoordinatesViewComponent implements AfterViewInit {
 
   drawLine(line: any[]): void {
     this.ctx.beginPath();
-    this.ctx.moveTo(0, 0);
     for (let i = 1; i <= line.length; i++) {
-      this.ctx.lineTo(i*50, line[i]);
-      this.ctx.moveTo(i*50, line[i]);
+      this.ctx.lineTo(i*100, line[i]);
+      this.ctx.moveTo(i*100, line[i]);
       this.ctx.stroke();
+      this.ctx.strokeStyle = "#0000ff";
       this.ctx.closePath();
     }
   }
